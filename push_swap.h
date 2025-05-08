dd010b3e3c910f193de2dc5782d1b33ba5cef56e
Ishta <ishtnabakka@gmail.com>
2025-05-08 14:07:50 +0300

    starting push_swap

---
+++ push_swap.h
@@ -0,0 +1,20 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <unistd.h>
+
+typedef	struct s_stack_node
+{
+	int		value;
+	int		current_position;
+	int		final_index;
+	int		push_cost;
+	bool	above_middle;
+	bool	cheapest;
+	struct	s_stack_node *target_node;
+	struct	s_stack_node *next;
+	struct	s_stack_node *prev;
+}	t_stack_node;
